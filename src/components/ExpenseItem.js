eb8125a208c954546d73168f7ceecfc54e063a1e
athateyshyam <athateyshyam@gmail.com>
2022-04-21 17:08:21 +0530

    Outputting daynamic data and working with expressions in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
-    return (<div className="expense-item">
-        <div>March 21,2021</div>
-        <div className="expense-item__description">
-            <h2>Car Insurance</h2>
-            <div className="expense-item__price">$2458.65</div>
+    const expenseDate=new Date(2022,4,21);
+    const expenseTitle='Car Insurance';
+    const expensePrice=245.36;
+
+    return (<div className='expense-item'>
+        <div>{expenseDate.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item__price'>${expensePrice}</div>
         </div>
     </div>);
 }
