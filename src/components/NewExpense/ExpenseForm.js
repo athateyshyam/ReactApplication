5fbde5167ac635aeb24bdf784a3634504660ae1e
athateyshyam <athateyshyam@gmail.com>
2022-04-25 13:00:42 +0530

    Recommended approach while working with one state rather than multiple states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -13,23 +13,40 @@ const ExpenseForm = () => {
 
     const titleChangeHadler = (event) => {
         //setEnteredTitle(event.target.value)
+        /*
         setUserInput({
             ...userInput,
             enteredTitle: event.target.value
         });
+        */
+       //Use this approach over above one to change state as it always garuntees previous state is latest updated one.
+       setUserInput((prevState)=>{
+           return {...prevState,enteredTitle: event.target.value};
+       });
     }
     const amountChangeHandler = event => {
         //setEnteredAmount(event.target.value)
+        /*
         setUserInput({
             ...userInput,
             enteredAmount: event.target.value
         });
+        */
+        setUserInput((prevState)=>{
+            return {...prevState,enteredAmount: event.target.value};
+        });
+
     }
     const dateChangeHandler = event => {
         //setEnteredDate(event.target.value)
+        /*
         setUserInput({
             ...userInput,
             enteredDate: event.target.value
+        });
+        */
+        setUserInput((prevState)=>{
+            return {...prevState,enteredDate: event.target.value};
         });
     }
 
