af809a8886b3109abfc6a27a296b6194c0d0bd03
athateyshyam <athateyshyam@gmail.com>
2022-04-26 14:04:57 +0530

    Code for adding new expense into array

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import Expenses from './components/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+const DUMMY_DEXPENSES = [
+  { id: 'e1', title: 'Car Insurance', amount: 294.35, date: new Date(2021, 3, 7) },
+  { id: 'e2', title: 'Mobile Recharge', amount: 30005.35, date: new Date(2021, 5, 12) },
+  { id: 'e3', title: 'Shopping', amount: 98745.35, date: new Date(2021, 12, 3) },
+  { id: 'e4', title: 'Vegitables', amount: 500.35, date: new Date(2022, 5, 3) },
+  { id: 'e5', title: 'Hoteling', amount: 1500.35, date: new Date(2021, 1, 4) },
+];
 
 function App() {
-  const expenses = [
-    { id: 'e1', title: 'Car Insurance', amount: 294.35, date: new Date(2021, 3, 7) },
-    { id: 'e2', title: 'Mobile Recharge', amount: 30005.35, date: new Date(2021, 5, 12) },
-    { id: 'e3', title: 'Shopping', amount: 98745.35, date: new Date(2021, 12, 3) },
-    { id: 'e4', title: 'Vegitables', amount: 500.35, date: new Date(2022, 5, 3) },
-    { id: 'e5', title: 'Hoteling', amount: 1500.35, date: new Date(2021, 1, 4) },
-  ];
-
+  const [expenses,setExpenses]=useState(DUMMY_DEXPENSES);
   const addExpenseHandler = expense =>{
-    console.log('In App.js file!');
-    console.log(expense);
+    setExpenses(prevExpense=>{
+      return [expense,...prevExpense];
+    });
   }
   return (
     <div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
