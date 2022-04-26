79db7230ea3a59561c597ca9b4b859205f4e6327
athateyshyam <athateyshyam@gmail.com>
2022-04-26 13:42:04 +0530

    Code for redering list of data dynamically using map()

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,18 +4,12 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    const [title,setTitle] = useState(props.title);
-    const clickHandler=()=>{
-        setTitle('Updated!');
-        console.log('Clicked!!!');
-    }
     return (<Card className='expense-item'>
         <ExpenseDate date={props.date} />
         <div className='expense-item__description'>
-            <h2>{title}</h2>
+            <h2>{props.title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
         </div>
-        <button onClick={clickHandler}>ClickMe</button>
     </Card>);
 }
 export default ExpenseItem;
